1c98259566640f18328eac01694325343290e9f4
Rafaela Moreira de Almeida <Rafaela50489626@edu.df.senac.br>
2024-11-21 09:30:49 -0300

    jojo

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEditor;
+
+public class Menu : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("fase 1");
+    }
+
+    public void SairGame()
+    {
+        Application.Quit();
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("fase 1");
+    }
+    public void Replay2()
+    {
+        SceneManager.LoadScene("fase 2");
+    }
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Créditos");
+    }
+
+    public void VoltarMenu()
+    {
+        /*if(playSource != null)
+        {
+            playSource.PlayOneShot(play);
+        }*/
+        
+        SceneManager.LoadScene("Menu");
+
+    }
+}
